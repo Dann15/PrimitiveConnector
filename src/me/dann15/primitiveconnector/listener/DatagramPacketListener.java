ca3d13e0f909bc3c120906bb70163614f1e8cae9
Daniel Shirvani <danielshirv@gmail.com>
2019-12-29 23:03:22 -0800

    Delete DatagramPacketListener.java

--- src/me/dann15/primitiveconnector/listener/DatagramPacketListener.java
+++
@@ -1,9 +0,0 @@
-package me.dann15.primitiveconnector.listener;
-
-import java.net.DatagramPacket;
-
-public interface DatagramPacketListener {
-
-	void recievedDatagramPacket(DatagramPacket packet, byte[] buffer);
-	
-}
